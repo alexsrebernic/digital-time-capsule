930a320a27a18dccdf30bcc8784e7b43d40b94ac
Aylen Reynoso <areynoso@fi.uba.ar>
2024-09-11 12:46:57 -0300

    initialize instruction and tests added

--- contract/programs/contract/src/lib.rs
+++ contract/programs/contract/src/lib.rs
@@ -1,4 +1,8 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{token::{TokenAccount, Mint, Token}, associated_token::AssociatedToken};
+use mpl_token_metadata::instructions::{CreateMasterEditionV3CpiBuilder,CreateMetadataAccountV3CpiBuilder};
+use mpl_token_metadata::types::{Creator, DataV2, };
+use mpl_token_metadata::accounts::{Metadata, MasterEdition};
 
 declare_id!("DWScEV42ig3zGpZUhVXtuV2BzwQ4oxnFeXiBdZB6uDaZ");
 
@@ -6,11 +10,31 @@ declare_id!("DWScEV42ig3zGpZUhVXtuV2BzwQ4oxnFeXiBdZB6uDaZ");
 pub mod contract {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize_capsule_machine(ctx: Context<InitializeMachine>) -> Result<()> {
+        let capsule_machine: &mut Account<CapsuleMachine> = &mut ctx.accounts.capsule_machine;
+        capsule_machine.count = 0; 
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeMachine<'info> {             
+    #[account(init, payer=user, space = 8 + 8)] //TODO: check allocation space
+    pub capsule_machine: Account<'info, CapsuleMachine>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+//------------Accounts------------------
+#[account]
+pub struct CapsuleMachine{
+    pub count: u64,
+}
+
+//------------Errors------------------
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Access denied. The opening date has not been reached.")]
+    AccessDenied,
+}
--- contract/tests/contract.ts
+++ contract/tests/contract.ts
@@ -1,16 +1,42 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Contract } from "../target/types/contract";
+import { SystemProgram, Connection } from "@solana/web3.js";
+import { assert } from "chai";
 
 describe("contract", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const connection = new Connection("http://127.0.0.1:8899", "confirmed");
+  const provider = anchor.AnchorProvider.env();
 
   const program = anchor.workspace.Contract as Program<Contract>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  const capsuleMachine = anchor.web3.Keypair.generate();
+  const user = provider.wallet;
+
+  it("should initialize the capsule machine", async () => {
+    const init_accounts = {
+      capsuleMachine: capsuleMachine.publicKey,
+      user: user.publicKey,
+      systemProgram: SystemProgram.programId,
+    };
+
+    const tx = await program.methods
+      .initializeCapsuleMachine()
+      .accounts(init_accounts)
+      .signers([capsuleMachine])
+      .rpc();
+
     console.log("Your transaction signature", tx);
+
+    // Fetch the capsule machine and check its state
+    const capsuleMachineAccount = await program.account.capsuleMachine.fetch(
+      capsuleMachine.publicKey
+    );
+    assert.equal(
+      capsuleMachineAccount.count.toNumber(),
+      0,
+      "Capsule machine count should be 0"
+    );
   });
 });
