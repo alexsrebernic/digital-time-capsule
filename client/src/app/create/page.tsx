fe2100598359c4e828572ff4f0b8c4e994de1b1f
alexsrebernic <84547550+alexsrebernic@users.noreply.github.com>
2024-09-11 19:19:10 -0300

    Commit before rebase

--- client/src/app/create/page.tsx
+++ client/src/app/create/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { DigitalTimeCapsuleForm } from "@/components/interfaces/digital-time-capsule-form";
 import { Button } from "@/components/ui/button";
